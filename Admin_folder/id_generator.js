e08da78a353cd3357d37cb6c696dcd0a10421d95
LazyNumble1 <amjkn10009@gmail.com>
2026-02-22 22:35:56 +0800

    Add new colors

--- Admin_folder/id_generator.js
+++ Admin_folder/id_generator.js
@@ -1,6 +1,7 @@
 const patient_id = document.getElementById("patient_id");
 const submit = document.getElementById("submit");
 
+//for patientM
 patient_id.addEventListener("click", function () {
     patient_id.value = generateId12();
 });
@@ -17,5 +18,25 @@ function generateId12() {
         .padStart(4, "0");
 
     return timePart + randomPart; // 12 digits
+    
+};
+
+//for appointment_add
+patient_id.addEventListener("click", function () {
+    patient_id.value = generateId11();
+});
+
+submit.addEventListener("click", function () {
+    window.alert("database not connected");
+    window.location.href = "appointment_add.html"
+});
+
+function generateId11() {
+    const timePart = Date.now().toString().slice(-7); // last 7 digits of timestamp
+    const randomPart = Math.floor(Math.random() * 10101)
+        .toString()
+        .padStart(4, "0");
+
+    return timePart + randomPart; // 11 digits
     
 };
